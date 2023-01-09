42b70615cc33b7f94fc35312e8ba8de9a94bcf56
AndreaMatarrese <matarreseandrea47@gmail.com>
2023-01-09 17:19:30 +0100

    changes

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<!-- Oggi pomeriggio ripassate i primi concetti di classe e di variabili e
+metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
+- è definita una classe ‘Movie’
+   => all'interno della classe sono dichiarate delle variabili d'istanza
+   => all'interno della classe è definito un costruttore
+   => all'interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +14,31 @@
     <title>Php-oop</title>
 </head>
 <body>
-    
+    <?php
+        class Movies {
+            public $title;
+            public $genre;
+            public $year;
+            public $duration;
+
+            // metodo per la durata
+            public function setDuration($duration){
+                if($duration > 0 && $duration < 400){
+                    $this->duration = $duration; 
+                } else{
+                    echo 'durata film non corretta';
+                }
+            }
+            public function getTitle($title_film1){
+                return $this->title;
+            }
+            // costruttore
+
+
+        }
+        $film1 = new Movies();
+        $title_film1 = $film1->getTitle($title_film1);
+        echo $title_film1;
+    ?>
 </body>
 </html>
